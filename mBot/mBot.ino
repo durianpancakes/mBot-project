ba8004bb25f67dbbe8cf55183005eef49db5d713
chanxuhui <56961189+durianpancakes@users.noreply.github.com>
2019-11-06 10:31:32 +0800

    updated code on movement based on color

--- mBot/mBot.ino
+++ mBot/mBot.ino
@@ -17,6 +17,8 @@ void setup() {
 void loop() {
   int sensorState;
   while(1){ // No black line encountered
+    Serial.print(analogRead(A0));
+    Serial.print("\n");
     sensorState = lineFinder.readSensors();
     // When obstacle is encountered, stop the mBot
     if(ultrasonic_3.distanceCm() < 7){
@@ -26,26 +28,56 @@ void loop() {
     else if(sensorState == S1_IN_S2_IN){
       move(0, 0);
       // Function to detect color
-         // If red is detected
-         // If light blue is detected, two successive right-turns in two grids
-         // If purple is detected, two successive left-turns in two grids
+         // If red is detected, turn left
+         if(color == RED){
+            move(3, 100);
+         }
          // If green is detected, turn right
+         else if(color == GREEN){
+            move(4, 100);
+         }
+         // If light blue is detected, two successive right-turns in two grids
+         else if(color == LIGHT BLUE){
+            while(ultrasonic_3.distanceCm() < 7){
+              move(4, 100);
+            }
+            while(ultrasonic_3.distanceCm() > 7){
+              avoid_obstacle();
+            }
+            while(ultrasonic_3.distanceCm() < 7){
+              move(4, 100);
+            }
+         }
+         // If purple is detected, two successive left-turns in two grids
+         else if(color == PURPLE){
+            while(ultrasonic_3.distanceCm() < 7){
+              move(3, 100);
+            }
+            while(ultrasonic_3.distanceCm() > 7){
+              avoid_obstacle();
+            }
+            while(ultrasonic_3.distanceCm() < 7){
+              move(3, 100);
+            }
+         }
          // If yellow is detected, U-turn in the same grid
+         else if(color == YELLOW){
+            while(ultrasonic_3.distanceCm() < 7){
+              move(3, 100);
+            }
+         }
          // If black is detected, fire microphone
+         else if(color == BLACK){
             // If sound is detected, check frequency
               // If frequency is between 100 and 300Hz, turn left
               // If frequency is 3000Hz and above, turn right
             // If no sound detected, end of maze.
-    }
-    else if(analogRead(A0) < 300){ // Left obstacle, turn right
-      move(4, 100);
-    }
-    else if(analogRead(A1) < 280){ // Right obstacle, turn left
-      move(3, 100);
+         }
     }
     else{
-      move(1, 100);
+        avoid_obstacle();
     }
+    
   }
 }
 
@@ -72,4 +104,17 @@ void move(int direction, int speed){
   }
   motor_9.run((9) == M1 ? -(leftSpeed) : (leftSpeed));
   motor_10.run((10) == M1 ? -(rightSpeed) : (rightSpeed));
+}
+
+void avoid_obstacle(){
+    if(analogRead(A0) < 420){ // Left obstacle, turn right
+      move(4, 100);
+    }
+    else if(analogRead(A1) < 150){ // Right obstacle, turn left
+      move(3, 100);
+    }
+    else{
+       motor_9.run(-1 * (50 / 100.0 * 255));
+       motor_10.run(56 / 100.0 * 255);
+    }
 }
