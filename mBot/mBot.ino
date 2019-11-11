4f4c9b676f7a23002a030543f930c73f6d94120d
chanxuhui <56961189+durianpancakes@users.noreply.github.com>
2019-11-11 18:11:46 +0800

    too many changes for me to list
    changes

--- mBot/mBot.ino
+++ mBot/mBot.ino
@@ -5,11 +5,17 @@
 #define LDRWait 10
 #define LDR A6
 #define RGBWait 200
+#define DCTurnWait 500
+#define DCUTurnWait 1050
+#define debug 1
+#define buzzer 8
 
-//floats to hold colour arrays
+
+// Floats to hold colour arrays
 float colourArray[] = {0,0,0};
-float blackArray[] = {346,339,297};
-float greyDiff[] = {635,644,680};
+float blackArray[] = {121,116,109};
+float greyDiff[] = {547,563,488};
+float soundArray[] = {0,0,0,0};
 
 MeRGBLed rgbled_7(7, 2);
 MeUltrasonicSensor ultrasonic_3(3);
@@ -21,75 +27,87 @@ void setup() {
   Serial.begin(9600);
   pinMode(A0, INPUT); // Left IR
   pinMode(A1, INPUT); // Right IR
+  pinMode(A2, INPUT); // High-pass filter
+  pinMode(A3, INPUT); // Band-pass filter
 }
 
 void loop() {
-  int sensorState, color;
+  int sensorState, color, sound;
   int n;
   while(1){ // No black line encountered   
     sensorState = lineFinder.readSensors();
-    Serial.print("A0 = ");
-    Serial.print(analogRead(A0));
+    Serial.print("sound = ");
+    Serial.print(analogRead(A1));
     Serial.print("\n");
 
     // Encountered black line
     if(sensorState == S1_IN_S2_IN){
       move(0, 0);
       color = colourSensor();
-      Serial.print("Action = ");
-      Serial.print(color);
       // Function to detect color
-         
          // If green is detected, turn right
          if(color == 1){
             move(4, 100);
-            delay(950);
+            delay(DCTurnWait);
          }
          
          // If red is detected, turn left
          else if(color == 2){
             move(3, 100);
-            delay(950);
+            delay(DCTurnWait);
          }
          
          // If light blue is detected, two successive right-turns in two grids
          else if(color == 4){
             move(4, 100);
-            delay(950);
+            delay(DCTurnWait);
             while(ultrasonic_3.distanceCm() > 7){
               avoid_obstacle();
             }
             move(4, 100);
-            delay(950);
+            delay(DCTurnWait);
          }
          // If purple is detected, two successive left-turns in two grids
          else if(color == 5){
             move(3, 100);
-            delay(950);
+            delay(DCTurnWait);
             while(ultrasonic_3.distanceCm() > 7){
               avoid_obstacle();
             }
             move(3, 100);
-            delay(950);
+            delay(DCTurnWait);
          }
          // If yellow is detected, U-turn in the same grid
          else if(color == 3){
-            move(3, 100);
-            delay(1600);
+            move(4, 100);
+            delay(DCUTurnWait);
          }
+         
          // If black is detected, fire microphone
          else if(color == 0){
-            // If sound is detected, check frequency
-              // If frequency is between 100 and 300Hz, turn left
-              // If frequency is 3000Hz and above, turn right
-            // If no sound detected, end of maze.
             move(0,0);
+            // If sound is detected, check frequency
+            sound = read_sound();
+            // If frequency is between 100 and 300Hz, turn left
+            if(sound == 1){
+              move(3, 100);
+              delay(DCTurnWait);
+            }
+            // If frequency is 3000Hz and above, turn right
+            else if(sound == 2){
+              move(4, 100);
+              delay(DCTurnWait);
+            }
+            // If no sound detected, end of maze.
+            else{
+              move(0, 0);
+              // playtone();
+            } 
          }
     }
     else{
         avoid_obstacle();
     }
-    
   }
 }
 
@@ -122,14 +140,14 @@ void avoid_obstacle(){
     if(analogRead(A0) < 320){ // Left obstacle, turn right
       move(4, 100);
     }
-    else if(analogRead(A1) < 290){ // Right obstacle, turn left
+    else if(analogRead(A1) < 280){ // Right obstacle, turn left
       move(3, 100);
     }
     else if(ultrasonic_3.distanceCm() < 7){
       move(0, 0);
     }
     else{
-      move(1, 150);
+      move(1, 200);
     }
 }
 
@@ -145,6 +163,9 @@ void lightgreen() {
 void lightblue() {
   rgbled_7.setColor(0, 0,0,255);
   rgbled_7.show();
+}
+void lightyellow(){
+  rgbled_7.setColor(0, 220, 112, 175);
 }
 void lightoff() {
   rgbled_7.setColor(0, 0, 0, 0);
@@ -165,47 +186,57 @@ int getAvgReading(int times){
   return total/times;
 }
 
+int getAvgReading_sound(int times, int sound){      
+//find the average reading for the requested number of times of scanning LDR
+  int reading;
+  int total =0;
+//take the reading as many times as requested and add them up
+  for(int i = 0;i < times;i++){
+     reading = analogRead(sound);
+     total = reading + total;
+     delay(LDRWait);
+  }
+//calculate the average and return it
+  return total/times;
+}
 int colourSensor() {
-  
-     lightred();
-     delay(RGBWait);
-     colourArray[0] = getAvgReading(5);         //scan 5 times to take the average, 
-     lightoff();
-     delay(RGBWait);
-     lightblue();
-     delay(RGBWait);
-     colourArray[1] = getAvgReading(5);         //scan 5 times to take the average, 
-     lightoff();
-     delay(RGBWait);
-     lightgreen();
-     delay(RGBWait);
-     colourArray[2] = getAvgReading(5);         //scan 5 times to take the average, 
-     lightoff();
-     delay(RGBWait);
-     for(int c=0;c<=2;c++){ 
-      Serial.println("Colour = ");
+    lightred();
+    delay(RGBWait);
+    colourArray[0] = getAvgReading(5);         //scan 5 times to take the average, 
+    lightoff();
+    delay(RGBWait);
+    lightblue();
+    delay(RGBWait);
+    colourArray[1] = getAvgReading(5);         //scan 5 times to take the average, 
+    lightoff();
+    delay(RGBWait);
+    lightgreen();
+    delay(RGBWait);
+    colourArray[2] = getAvgReading(5);         //scan 5 times to take the average, 
+    lightoff();
+    delay(RGBWait);
+    for(int c=0;c<=2;c++){ 
       //Serial.println(int(colourArray[c]));
       colourArray[c] = (colourArray[c] - blackArray[c])/(greyDiff[c])*255;
       lightoff(); //turn off the current LED colour
-      Serial.println(int(colourArray[c])); //show the value for the current colour LED, which corresponds to either the R, G or B of the RGB code
-     }
-     if (colourArray[2]>colourArray[1]&& colourArray[2]>colourArray[0]&& colourArray[0]<100 && colourArray[1]<100 && colourArray[2]>50) //Green=1
-     {
-     return 1;
-     }
-     else if (colourArray[0]>colourArray[1]&& colourArray[0]>colourArray[2]&& colourArray[0]>100 && colourArray[2]<100 && colourArray[1]<100) //RED = 2
-     {
+    }
+    if (colourArray[2]>colourArray[1]&& colourArray[2]>colourArray[0]&& colourArray[0]<100 && colourArray[1]<100 && colourArray[2]>50) //Green=1
+    {
+      return 1;
+    }
+    else if (colourArray[0]>colourArray[1]&& colourArray[0]>colourArray[2]&& colourArray[0]>100 && colourArray[2]<100 && colourArray[1]<100) //RED = 2
+    {
       return 2;
-     }
+    }
     else if (colourArray[0]>colourArray[1]&& colourArray[0]>colourArray[2]&& colourArray[0]>200) //Yellow = 3
     {
       return 3;
     }
-    else if (colourArray[1]>colourArray[0]&& colourArray[1]>colourArray[2]&& colourArray[1]>150) // Light blue= 4
+    else if (colourArray[1]>colourArray[0]&& colourArray[1]>colourArray[2]&& colourArray[1]>160) // Light blue= 4
     {
       return 4;
     }
-    else if (colourArray[1]>colourArray[0]&& colourArray[1]>colourArray[2]&& colourArray[1]<200 && colourArray[0]>100 && colourArray[2]>100)//purple=5
+    else if (colourArray[1]>colourArray[0]&& colourArray[1]>colourArray[2]&& colourArray[1]<160 && colourArray[0]>75 && colourArray[2]>75)//purple=5
     {
       return 5;
     }
@@ -213,5 +244,19 @@ int colourSensor() {
     {
       return 0;
     }
-   
+}
+
+int read_sound(){
+  soundArray[0] = getAvgReading_sound(5, A2); // Get average high pass reading
+  soundArray[1] = getAvgReading_sound(5, A3); // Get average band pass reading
+    
+  if(soundArray[0] - soundArray[1] < 1 || soundArray[1] - soundArray[0] < 1){
+    return 0;
+  }
+  else if(soundArray[0] > soundArray[1]){
+    return 1;
+  }
+  else if(soundArray[0] < soundArray[1]){
+    return 2;
+  }
 }
